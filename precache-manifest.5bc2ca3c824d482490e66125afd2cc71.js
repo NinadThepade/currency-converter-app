a357a80689f8c491d6c0dac25b7fc9ff10e94a9e
ninadthepade <ninad.thepade@gmail.com>
2020-05-04 17:25:19 +0530

    Updates

---
+++ precache-manifest.5bc2ca3c824d482490e66125afd2cc71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125f43db05fd3f1bf8ab2f6633fdba92",
+    "url": "/currencyconverterapp/index.html"
+  },
+  {
+    "revision": "6de08f4a8c2d5b07117d",
+    "url": "/currencyconverterapp/static/css/main.b40a4c2a.chunk.css"
+  },
+  {
+    "revision": "d0243275fcc6a9e95751",
+    "url": "/currencyconverterapp/static/js/2.7384d55b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currencyconverterapp/static/js/2.7384d55b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de08f4a8c2d5b07117d",
+    "url": "/currencyconverterapp/static/js/main.9421f905.chunk.js"
+  },
+  {
+    "revision": "d058f2817834eb914401",
+    "url": "/currencyconverterapp/static/js/runtime-main.686ecd08.js"
+  }
+]);
